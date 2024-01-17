f713895b51237f5a790b1ef0042af49c0c9d2200
rafikawardah <rafika.wardah12@gmail.com>
2024-01-17 14:41:53 +0700

    Latihan Berpindah Fragment

--- app/src/main/java/com/example/latihanfragment/HomeFragment.kt
+++ app/src/main/java/com/example/latihanfragment/HomeFragment.kt
@@ -23,6 +23,15 @@ class HomeFragment: Fragment(), View.OnClickListener {
     }
 
     override fun onClick(v: View) {
+        if(v?.id == R.id.btn_category) {
+            val categoryFragment = CategoryFragment()
+            val fragmentManager = parentFragmentManager
+            fragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, categoryFragment, CategoryFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
+        }
 
     }
 
