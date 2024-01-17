7221db9fd02d2ba6210dbe2c47e37acdfa0891f7
rafikawardah <rafika.wardah12@gmail.com>
2024-01-17 20:05:48 +0700

    Memindahkan Data Antar Fragment

--- app/src/main/java/com/example/latihanfragment/CategoryFragment.kt
+++ app/src/main/java/com/example/latihanfragment/CategoryFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import com.example.latihanfragment.DetailCategoryFragment.Companion.EXTRA_NAME
 
 class CategoryFragment : Fragment(), View.OnClickListener {
     override fun onCreateView(
@@ -19,13 +20,31 @@ class CategoryFragment : Fragment(), View.OnClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val btnDetailCategory: Button = view.findViewById(R.id.btn_detail_category)
+        btnDetailCategory.setOnClickListener(this)
     }
 
     //Mengirim Data Antar Fragment
     override fun onClick(v: View?) {
         if(v?.id == R.id.btn_detail_category) {
+            val detailCategoryFragment = DetailCategoryFragment()
 
+            //Pembungkus bisa juga untuk kls map data string untuk obyek parcelable
+            //Maping = key & value
+            //dapat input >1 parameter/variabel ke dalam bundle
+            val bundle = Bundle()
+            bundle.putString(EXTRA_NAME, "Lifestyle")
+            val description = "Kategori ini akan berisi produk-produk lifestyle"
+
+            detailCategoryFragment.arguments = bundle
+            detailCategoryFragment.description = description
+
+            //Dapat membuat instance-instance baru
+            val fragmentManager = parentFragmentManager
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.frame_container, detailCategoryFragment, DetailCategoryFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
         }
     }
-
 }
